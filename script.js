f1a598275c32a0837ddb0b6a235e3ed9394240a8
G3rsonBR <gersons.br13@gmail.com>
2025-11-17 21:03:39 -0300

    feat: Project done

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
+  const mainNav = document.querySelector(".main-nav");
+
+  mobileMenuIcon.addEventListener("click", () => {
+    mainNav.classList.toggle("active");
+  });
+});
